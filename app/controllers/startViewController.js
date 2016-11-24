58654e74c6082b34fe06829d4ea954b9a79a1126
Alexandru Ursachi <alexandru.ursachi@basware.com>
2016-11-24 14:12:28 +0200

    add placeholder for username input

--- app/controllers/startViewController.js
+++ app/controllers/startViewController.js
@@ -3,7 +3,7 @@
 angular.module('words.start')
     .controller('StartViewCtrl', ['$scope', '$rootScope', '$location',
         function ($scope, $rootScope, $location) {
-            $scope.username = "Your name";
+            $scope.username = "";
             $scope.submit = function () {
                 //todo - is using root scope bad practice?
                 //todo - use a service (kind of hacky) or some kind of angular storage module (overhead) instead?
